e00e4261986f76c310bdc4aab6eef24b8e489b3b
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-07-15 16:22:36 +0100

    fix: add tailwind utilities, add routes to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
  import Signup from './Signups/Signup.jsx';
  import LoginOwner from './components/LoginOwner'
  import LoginEmployee from './components/LoginEmployee'
@@ -6,10 +7,14 @@ import React from 'react';
  const App = () => {
    return (
      <>  
-     <Signup />
-    <LoginOwner />
-    <LoginEmployee />
-   
+     <Router>
+      <Routes>
+          <Route path='/' element= {<Signup />} />
+          <Route path='/Employee' element= {<LoginEmployee />} />
+          <Route path='/Owner' element= {<LoginOwner />} />
+      </Routes>
+     </Router>
+    
      </>
    )
  }
