4a3be2d43323471d9013f3ca4afa2adb0bca02c6
ishahtech <salawudeenaishatolabisi@gmail.com>
2024-07-15 13:24:27 +0100

    feat: install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
  import Signup from './Signups/Signup.jsx';
  import LoginOwner from './components/LoginOwner'
  import LoginEmployee from './components/LoginEmployee'
@@ -6,10 +6,10 @@
  const App = () => {
    return (
      <>  
-    
+     <Signup />
     <LoginOwner />
     <LoginEmployee />
-    <Signup />
+   
      </>
    )
  }
