cc4823064581f9e9504877a884c92ba4607f1c6e
Joy Abel <joydorothy222@gmail.com>
2024-07-20 22:26:04 +0100

    fix: delete unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import KeyFeatures from "./Component/KeyFeatures";
 import "./index.css";
 
 const App = () => {
   return (
     <div className="App">
-      <KeyFeatures />
     </div>
   );
 };
