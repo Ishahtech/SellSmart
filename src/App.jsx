b8af9e92995d18af798f088318bf05ee9bb3bcbc
floxyp <onyekao163@gmail.com>
2024-07-22 20:58:31 +0100

    Feat: Add header and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import WelcomeScreen from './components/WelcomeScreen';  // Adjust the path as necessary
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  
 import LoginScreen from './components/LoginScreen/LoginScreen';  // Adjust the path as necessary
  import Signup from './Signups/Signup.jsx';
  import LoginOwner from './components/LoginOwner'
  import LoginEmployee from './components/LoginEmployee'
+ import LandinPage from './components/LandingPage.jsx'
  
  const App = () => {
    return (
      <>  
      <Router>
       <Routes>
-           <Route path="/" element={<WelcomeScreen />} />
+           
           <Route path="/login" element={<LoginScreen />} />
           <Route path='/Signup' element= {<Signup />} />
           <Route path='/Employee' element= {<LoginEmployee />} />
           <Route path='/Owner' element= {<LoginOwner />} />
       </Routes>
      </Router>
+     <LandinPage/>
     
      </>
    )
