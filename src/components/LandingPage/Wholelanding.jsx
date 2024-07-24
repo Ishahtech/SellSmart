17f523e1b010609b96dc3455ad9ce3cda92d1d14
Janeirungu01 <jwirungu96@gmail.com>
2024-07-24 20:09:48 +0300

    feat: aboutus update

---
+++ src/components/LandingPage/Wholelanding.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header"
+import KeyFeatures from "./KeyFeatures"
+import AboutUs from "../AboutUs"
+
+function Wholelanding() {
+  return (
+    <>
+    <Header />
+    <KeyFeatures />
+   
+    </>
+  )
+}
+
+export default Wholelanding
