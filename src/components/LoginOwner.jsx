289cf8947b9434babd6639d3a4ca00342c1474a6
Joy Abel <joydorothy222@gmail.com>
2024-09-18 15:50:29 +0100

    refactore

--- src/components/LoginOwner.jsx
+++ src/components/LoginOwner.jsx
@@ -4,7 +4,6 @@ import EmailCodeField from "./EmailField";
 import SendOTP from "./SendOTP";
 
 const LoginOwner = () => {
-
   return (
     <>
       <div className="flex justify-center items-center min-h-screen bg-gray-200 py-12 px-4 sm:px-6 lg:px-8">
